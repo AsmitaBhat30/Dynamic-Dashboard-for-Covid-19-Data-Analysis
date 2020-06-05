493a9f280743c4c20393eba0831d33480537e68c
abhat <abhat@rhrk.uni-kl.de>
2020-06-05 18:24:54 +0200

    Business Understanding and Data Gathering phases Version 1.0 : Data gathered using Webscraping and REST API Calls

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='We would like to explore the spread of coronavirus across different countries.',
+    author='Asmita Bhat',
+    license='',
+)
